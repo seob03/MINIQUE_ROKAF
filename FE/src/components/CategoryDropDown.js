19ca7a7e54155ccef8618c1a4c7e62718f7ff5aa
hotte00 <junpio0812@kaist.ac.kr>
2025-03-09 20:32:10 +0900

    Backup Push (HJ)

--- FE/src/components/CategoryDropDown.js
+++ FE/src/components/CategoryDropDown.js
@@ -8,19 +8,11 @@ function CategoryDropDown(props) {
     const menuItems1 = ['GIRL', 'BOY'];
     const menuItems2 = ['OUTER', 'TOP', 'BOTTOM', 'SHOES', 'ETC']
 
-<<<<<<< HEAD
-    return (
-        <div style={{ display: 'flex', marginTop: '12px' }}>
-            <div
-                className="CategoryDropDown-Box"
-                onClick={() => setDrop1(!isDrop1)}
-=======
     return(
         <div style={{display: 'flex', marginTop: '12px'}}>
             <div 
                 className={`CategoryDropDown-Box ${(props.isActive_1) ? '':'inactive'}`}
                 onClick={()=>{setDrop1(!isDrop1); console.log('드롭다운 : ',isDrop1, '둘다 :',isDrop1 && props.isActive_1)}}
->>>>>>> 30c23d5d9d82190b24d78069b18fcdba81457de4
             >
                 {
                     (props.isActive_1)? 
@@ -44,11 +36,7 @@ function CategoryDropDown(props) {
                     </div> :null
                 }
             </div>
-<<<<<<< HEAD
-            <img src='/img/Category_Arrow.svg' style={{ marginLeft: '44px', marginRight: '44px' }} />
-=======
             <img src='/img/Category_Arrow.svg' style={{marginLeft: '30px', marginRight: '30px'}}/>
->>>>>>> 30c23d5d9d82190b24d78069b18fcdba81457de4
             {
                 (props.상위카테고리 != '') &&
                 <div
